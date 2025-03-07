f2dbd66967375aa79c8a07c925a982f5798ce23e
Sapp00 <39338428+Sapp00@users.noreply.github.com>
2025-03-07 11:46:04 +0100

    Update shell.nix: now only sets cargo parameters on Darwin

--- rust/shell.nix
+++ rust/shell.nix
@@ -19,19 +19,21 @@ pkgs.mkShellNoCC {
   nativeBuildInputs = [ pkgs.pkg-config ];
 
   shellHook = ''
-    export LDFLAGS="-L${pkgs.libiconv}/lib -L${pkgs.openssl.dev}/lib -liconv"
-    export PKG_CONFIG_PATH="${pkgs.libiconv}/lib/pkgconfig:${pkgs.openssl.dev}/lib/pkgconfig:$PKG_CONFIG_PATH"
-    export LD_LIBRARY_PATH="${pkgs.libiconv}/lib:${pkgs.openssl.dev}/lib:$LD_LIBRARY_PATH"
-    
-    # Add both the system and Nix-provided framework paths to RUSTFLAGS
-    export RUSTFLAGS="-L${pkgs.libiconv}/lib -L${pkgs.openssl.dev}/lib -liconv \
-      -C link-arg=-F/System/Library/Frameworks \
-      -C link-arg=-F$(nix-build '<nixpkgs>' -A darwin.apple_sdk.frameworks.Security)/Library/Frameworks \
-      -C link-arg=-framework -C link-arg=Security"
-
-    echo "LDFLAGS: $LDFLAGS"
-    echo "PKG_CONFIG_PATH: $PKG_CONFIG_PATH"
-    echo "LD_LIBRARY_PATH: $LD_LIBRARY_PATH"
-    echo "RUSTFLAGS: $RUSTFLAGS"
+    if [[ "$(uname)" == "Darwin" ]]; then
+      export LDFLAGS="-L${pkgs.libiconv}/lib -L${pkgs.openssl.dev}/lib -liconv"
+      export PKG_CONFIG_PATH="${pkgs.libiconv}/lib/pkgconfig:${pkgs.openssl.dev}/lib/pkgconfig:$PKG_CONFIG_PATH"
+      export LD_LIBRARY_PATH="${pkgs.libiconv}/lib:${pkgs.openssl.dev}/lib:$LD_LIBRARY_PATH"
+      
+      # Add both the system and Nix-provided framework paths to RUSTFLAGS
+      export RUSTFLAGS="-L${pkgs.libiconv}/lib -L${pkgs.openssl.dev}/lib -liconv \
+        -C link-arg=-F/System/Library/Frameworks \
+        -C link-arg=-F$(nix-build '<nixpkgs>' -A darwin.apple_sdk.frameworks.Security)/Library/Frameworks \
+        -C link-arg=-framework -C link-arg=Security"
+  
+      echo "LDFLAGS: $LDFLAGS"
+      echo "PKG_CONFIG_PATH: $PKG_CONFIG_PATH"
+      echo "LD_LIBRARY_PATH: $LD_LIBRARY_PATH"
+      echo "RUSTFLAGS: $RUSTFLAGS"
+    fi
   '';
 }
